9430c647cfae5c5bea7514a32a79d190a94bf3df
Emmanuel Temprano <tempra.8994@gmail.com>
2022-06-20 15:21:31 -0300

    Ciclos

---
+++ java.js
@@ -0,0 +1,8 @@
+// SOLICITUD DE DATOS PERSONALES
+let entrada = prompt("Ingrese sus datos personales")
+
+while(entrada != "salir"){
+    alert("El usuario ingreso " + entrada)
+
+    entrada = prompt("Ingrese mas datos importantes")
+}
